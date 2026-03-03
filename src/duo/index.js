e52516126fa30654042bb634bd2131280132ad4b
itslokeshx <lokeshvijayraina@gmail.com>
2026-03-04 01:07:58 +0530

    feat(duo): add DuoHeartbeat component for partner connection indication

---
+++ src/duo/index.js
@@ -0,0 +1,9 @@
+// Barrel export for all Duo components and hooks
+export { DuoButton } from "./DuoButton.jsx";
+export { DuoModal } from "./DuoModal.jsx";
+export { DuoPanel } from "./DuoPanel.jsx";
+export { DuoReactions } from "./DuoReactions.jsx";
+export { DuoEndCard } from "./DuoEndCard.jsx";
+export { DuoHeartbeat } from "./DuoHeartbeat.jsx";
+export { useDuo } from "./useDuo.js";
+export { useDuoStore } from "./duoStore.js";
